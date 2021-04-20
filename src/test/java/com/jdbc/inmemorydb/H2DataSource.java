cc3b7eace9a96851e27b49854673c72f3210f996
Dmitrii Barsuk <69538187+forbrock@users.noreply.github.com>
2021-04-20 14:17:59 +0300

    Update H2DataSource.java

--- src/test/java/com/jdbc/inmemorydb/H2DataSource.java
+++ src/test/java/com/jdbc/inmemorydb/H2DataSource.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class H2DataSource {
     private static final String URL = "jdbc:h2:mem:dbtest;DB_CLOSE_DELAY=-1";
 
+    // test commit, should be deleted
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
     }
